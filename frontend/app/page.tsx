5c0c429599304ef0c8ee1eac4a21130ac818ab39
SagarTS <sagartshrestha@gmail.com>
2023-10-21 19:29:33 +0545

    fix: integrate API for get all todos

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,27 +1,67 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { API } from '../services/api';
+
+interface todoData {
+  todo: string;
+}
 
 export default function Home() {
   const [inputText, setInputText] = useState('');
+  const [data, setData] = useState<todoData[] | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const handleTodoPost = async () => {
     await API.post('/todos', { todo: inputText });
+    setInputText('');
+    fetchData();
   };
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const response = await fetch('http://localhost:8000/todos');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (err) {
+      console.error('Error', err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
-      <h1>Todo</h1>
-      <div className='flex gap-5 p-5'>
-        <input
-          onChange={(e) => {
-            setInputText(e.target.value);
-          }}
-          placeholder='Todo Task'
-        />
-        <button onClick={handleTodoPost}>Tick</button>
-      </div>
+      {loading ? (
+        <p>LOADING...</p>
+      ) : (
+        <>
+          <h1>Todo</h1>
+          <div className='flex gap-5 p-5'>
+            <input
+              onChange={(e) => {
+                setInputText(e.target.value);
+              }}
+              placeholder='Todo Task'
+            />
+            <button onClick={handleTodoPost}>Tick</button>
+          </div>
+
+          <div>
+            {data &&
+              data?.map((d, i) => (
+                <div key={i}>
+                  <p>{d.todo}</p>
+                </div>
+              ))}
+          </div>
+        </>
+      )}
     </main>
   );
 }
